f633126d09b13748c3316fda8e4918e2d2b4ad58
jmiras <jm@weareindeed.com>
2025-02-21 11:44:47 +0800

    added for menu

--- assets/theme/controllers/SubMenuController.ts
+++ assets/theme/controllers/SubMenuController.ts
@@ -2,8 +2,44 @@ import { Controller } from "@hotwired/stimulus";
 import $ from 'jquery';
 
 export default class extends Controller {
-    static targets = ['trigger', 'submenu', 'header'];
+    static targets = ["header","menuContainer"];
 
-    connect() {
+    public static values  = {
+        mainMenuOpen: {type: Boolean, default: false}
     }
+    connect() {
+        this.navItems = document.querySelectorAll(".nav-item.has-submenu");
+
+        this.navItems.forEach(item => {
+            item.addEventListener("mouseenter", this.expandHeader.bind(this));
+            item.addEventListener("mouseleave", this.resetHeader.bind(this));
+        });
+    }
+
+    expandHeader() {
+        if (this.hasHeaderTarget) {
+            this.headerTarget.style.height = "350px";
+        }
+    }
+
+    resetHeader() {
+        if (this.hasHeaderTarget) {
+            this.headerTarget.style.height = "";
+        }
+    }
+
+    disconnect() {
+        this.navItems.forEach(item => {
+            item.removeEventListener("mouseenter", this.expandHeader.bind(this));
+            item.removeEventListener("mouseleave", this.resetHeader.bind(this));
+        });
+    }
+    button() {
+        this.mainMenuOpenValue = !this.mainMenuOpenValue;
+    }
+    mainMenuOpenValueChanged()
+    {
+        this.mainMenuOpenValue ? $(this.element).addClass('menu-open') : $(this.element).removeClass('menu-open');
+    }
+
 }
